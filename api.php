6ed21e16b910010be9e6cfc50d9bf47f3802a618
Julian Ignacio Garcia <juliangarcia.edi@gmail.com>
2021-05-08 14:44:56 -0300

    :bug: bugfix

--- api.php
+++ api.php
@@ -1,10 +1,16 @@
 <?php
   include_once 'secrets.php';
+  include './client/index.html';
   header("Access-Control-Allow-Origin: *");
 
   $secret = new Secret();
 
   $con = mysqli_connect("127.0.0.1", "root", "", "SECRETS_DB");
+
+  function home() {
+    header('Content-type: application/json');
+
+  }
 
   if ($con) {
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
@@ -12,7 +18,11 @@
     } else {
       if ($_SERVER['REQUEST_METHOD'] == "GET") {
         $_link = $_GET["link"];
-        $json = $secret->get_secret($con, $_link);
+        if ($_link) {
+          $json = $secret->get_secret($con, $_link);
+        } else {
+          home();
+        }
       } else {
         $json = array("status" => 0, "Info" => "Request method not accepted!");
       }
