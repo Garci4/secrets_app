a7f30c3727832af4e5e836045138140ea34c8e8e
Julian Ignacio Garcia <juliangarcia.edi@gmail.com>
2021-05-08 15:04:40 -0300

    :truck: Rearrange files

--- api.php
+++ api.php
@@ -1,17 +1,11 @@
 <?php
   include_once 'secrets.php';
-  include './client/index.html';
   header("Access-Control-Allow-Origin: *");
-
+  
   $secret = new Secret();
-
+  
   $con = mysqli_connect("127.0.0.1", "root", "", "SECRETS_DB");
-
-  function home() {
-    header('Content-type: application/json');
-
-  }
-
+  
   if ($con) {
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
       $json = $secret->post_secret($con, $_POST);
@@ -20,8 +14,6 @@
         $_link = $_GET["link"];
         if ($_link) {
           $json = $secret->get_secret($con, $_link);
-        } else {
-          home();
         }
       } else {
         $json = array("status" => 0, "Info" => "Request method not accepted!");
